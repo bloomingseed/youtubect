420b1d4a1da86111c4c1f4e347e0d5e34b5651a0
bloomingseed <nhattungnguyen.2kgl@gmail.com>
2021-06-03 21:59:50 +0700

    basic structure

--- background.js
+++ background.js
@@ -1,84 +1,65 @@
-
-function initMeta(){
-    if(globalThis.meta==undefined){
-        globalThis.meta = { spreadSheetUrl:"https://docs.google.com/spreadsheets/d/1oHExaxzet-1_2Jm3_vi1tasZjLQLl1dVozOnlb-BEmI",
-                            spreadSheetTabId:undefined,
-                            spreadSheetRequest:undefined,
-                            tabsList:[] };
+function isYoutubeUrlMatchFormat(url,output){
+    let parts = url.split('?');
+    if(parts.length!=2) return false;
+    if(parts[0]!='https://www.youtube.com/watch') return false;
+    let q = new URLSearchParams(parts[1]);
+    let arr = ['timeout','comment','row','col'];
+    for(let key of arr){
+        if(q.get(key)==null) return false;
+        output[key] = q.get(key);
     }
-}    
-/**
- * Called when new tab is created
- * params: tab: tab object that is created.
- */
-function onTabCreatedListener(tab){
-    try{
-        initMeta();
-    } catch(e){console.log(e);}
-    globalThis.meta.tabsList.push(tab.id);
+    return true;
+}
+function getTabListInstance(){
+    if(globalThis.tabList==undefined){
+        globalThis.tabList = [];
+    }
+    return globalThis.tabList;
 }
 /**
  * Called when a tab gets updated (refreshed, navigated, or internal updates?)
  */
 function onTabUpdatedListener(tabId,changeInfo,tab){
-    try{
-        initMeta();
-    } catch(e){console.log(e);}
-    console.log(`Tab ${tabId} updated`)
-    if(tab.url.indexOf(meta.spreadSheetUrl)>-1){
-        console.log(`Detected our spreadsheet at tab#${tabId}.`)
-        globalThis.meta.spreadSheetTabId=tabId;     // sets the current spreadsheet tab's ID
+    console.log(`TabId ${tabId}, Url ${tab.url}: updated.`);
+    let output = {};
+    let url = tab.url;
+    if(!isYoutubeUrlMatchFormat(url,output)) return;    // ensures this url matches our format
+    let tabList = getTabListInstance();
+    if(tabList.indexOf(tabId)!=-1) return;  // ensures this tab id is not listed
+    tabList.push(tabId);    // adds this tab id to list
+    chrome.scripting.executeScript({    // inject content.js script
+        target:{tabId},
+        files: ['content.js']
+    });
+    console.log(`TabId ${tabId}, Url ${tab.url}: script injected.`);
+}
+function isMessageMatchFormat(msg){
+    let arr = ['commentUrl','row','col'];
+    for(let key of arr){
+        if(msg[key]==undefined) return false;
     }
-    if(globalThis.meta.spreadSheetRequest &&    // checks if the spreadsheet request has been initialized
-        tab.url === globalThis.meta.spreadSheetRequest.url){     // checks if it's the requested url
-        console.log('Found requested Youtube URL.');
-        chrome.scripting.executeScript({    // inject content.js script
-            target:{tabId},
-            files: ['content.js']
-        });
-        console.log('Injected content script.')
-    }
-    if(tab.url.indexOf('https://www.youtube.com/')>-1){
-        console.log("Found youtube tab");
-        console.log(tab.url,tab);
-    }
+    return true;
 }
 /**
- * Called when the extension side receives a message from its content script
+ * Called when receives message from a tab
  */
 function onMessageListener(request, sender, sendResponse){
-    console.log(request);
-    // sets spreadsheet request
-    if(sender.id == globalThis.meta.spreadSheetTabId){
-        console.log('Received request from our spreadsheet.');
-        // let {url, timeout, comment} = request;
-        // globalThis.meta.spreadSheetRequest = {url,timeout,comment}
-    } else{
-        // sends comment url to spreadsheet tab
-        console.log('Received comment URL from requested Youtube URL.');
-        // chrome.tabs.sendMessage(globalThis.meta.spreadSheetTabId, request);
-    }
-
-}
-/**
- * Called when received a message sent to this extension ID
- */
-function onExternalMessageListener(request,sender,sendResponse){
-    console.log(request,sender);
-    if(sender.id == globalThis.meta.spreadSheetTabId){
-        console.log('Received request from our spreadsheet.');
-        // let {url, timeout, comment} = request;
-        // globalThis.meta.spreadSheetRequest = {url,timeout,comment}
-    }
+    let tabId = sender.tab.id;
+    console.log(`TabId ${tabId}: ${request}.`);    
+    if(!isMessageMatchFormat(request)) return;  // ensures sender's message contains our data
+    let tabList = getTabListInstance();
+    let indx = tabList.indexOf(tabId);  // gets index of this tabId in the list 
+    if(indx==-1) return;  // ensures this tab id is in the list
+    tabList.pop(indx);  // removes this tab id from the listed tabIds
+    let {commentUrl, row, col} = request;
+    // TODO: call GAS using Execution API here
 }
 // Events subscribing
 try{
-    chrome.tabs.onCreated.addListener(onTabCreatedListener);
     chrome.tabs.onUpdated.addListener(onTabUpdatedListener);
     chrome.runtime.onMessage.addListener(function(req,s,res){
-        if(s.tab) {
+        if(s.tab) {     // checks if message comes from a tab
             onMessageListener(req,s,res)
         }
     });
-    chrome.runtime.onMessageExternal.addListener(onExternalMessageListener);
 } catch(e){console.log(e);}
